255abab524d3a3993272d2375ca8bb28ca89334b
Theo <theo.gianella@proton.me>
2023-02-02 14:19:20 +0100

    refactoring board initialize methods

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -12,29 +12,38 @@ class Board
     ['A', 'B', 'C'].each do |row_letter|
       self.rows << Row.new(row_letter)
     end
-    #puts "Initialized rows : #{rows}"
   end
 
   def initialize_columns
     self.columns = []
     1.upto(3) do |col_number|
-      self.columns << []
-      rows.each do |row|
-        self.columns.last << row.squares[col_number - 1]
-      end
+      initialize_column(col_number)
     end
-    #puts "Initialized columns : #{columns}"
+  end
+
+  def initialize_column(col_number)
+    self.columns << []
+    rows.each do |row|
+      self.columns.last << row.squares[col_number - 1]
+    end
   end
 
   def initialize_diagonals
     self.diagonals = [[],[]]
+    initialize_first_diagonal
+    initialize_second_diagonal
+  end
+
+  def initialize_first_diagonal
     3.times do |i|
       self.diagonals[0] << rows[i].squares[i]
     end
+  end
+  
+  def initialize_second_diagonal
     3.times do |i|
       self.diagonals[1] << rows[2 - i].squares[i]
     end
-    #puts "Initialized diagonals : #{diagonals}"
   end
 
   def reset
